1ef1cac788b5a4359b4f4133716b0dad11b2e4a9
hecomi <info@hecomi.com>
2019-05-26 15:50:13 +0900

    tweak code.

--- Plugins/CustomTextureUpdate/src/dds_loader.cpp
+++ Plugins/CustomTextureUpdate/src/dds_loader.cpp
@@ -15,17 +15,17 @@ void DdsLoader::Load(const void *pData, size_t dataSize)
      // only DXT1 is supported now for the simplicity.
     if (pHeader->dwFourCC != 0x31545844 /* "1TXD" */) return;
 
-    m_width = pHeader->dwWidth;
-    m_height = pHeader->dwHeight;
+    width_ = pHeader->dwWidth;
+    height_ = pHeader->dwHeight;
 
     constexpr size_t headerSize = sizeof(DdsHeader);
-    const size_t blockWidth = (m_width + 3) / 4;
-    const size_t blockHeight = (m_height + 3) / 4;
+    const size_t blockWidth = (width_ + 3) / 4;
+    const size_t blockHeight = (height_ + 3) / 4;
     const size_t bufferSize = blockWidth * blockHeight * 8; 
     const auto *pBuffer = reinterpret_cast<const char*>(pData) + headerSize;
 
-    m_data = std::make_unique<char[]>(bufferSize);
-    std::memcpy(m_data.get(), pBuffer, bufferSize);
+    data_ = std::make_unique<char[]>(bufferSize);
+    std::memcpy(data_.get(), pBuffer, bufferSize);
 
-    m_hasLoaded = true;
+    hasLoaded_ = true;
 }
